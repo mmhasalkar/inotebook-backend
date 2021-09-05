63f8bad7599471b1d51e39e4c4c5a8cdb9d4e18d
Mukunda Mhasalkar <mukunda.mhasalkar@ignitesol.com>
2021-09-05 17:53:48 +0530

    Test indexes created using Collection.createIndex() to store unique email data

--- models/User.js
+++ models/User.js
@@ -21,4 +21,7 @@ const UserSchema = new Schema({
     }
 })
 
-module.exports = mongoose.model('user', UserSchema)
+User = mongoose.model('user', UserSchema)
+User.createIndexes()
+
+module.exports = User
--- routes/auth.js
+++ routes/auth.js
@@ -1,12 +1,29 @@
 const express = require('express');
 const User = require('../models/User');
+const { body, validationResult } = require('express-validator');
+
 const router = express.Router();
 
-router.post('/', (req, res) => {
-    const user = User(req.body)
+router.post('/', [
+    body('name', "Your name should at lease 2 characters long.").isLength({ min: 2 }),
+    body('password', "Your password should at lease 5 characters long.").isLength({ min: 5 }),
+    body('email', "Entered email is not valid.").isEmail()
+], (req, res) => {
+    // Finds the validation errors in this request and wraps them in an object with handy functions
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+        return res.status(400).json({ errors: errors.array() });
+    }
+
     console.log(req.body)
-    user.save()
-    res.json(req.body)
+    User.create({
+        name: req.body.name,
+        password: req.body.password,
+        email: req.body.email
+    }).then(user => res.json(user)).catch(err => {
+        console.log("error while creating user:", err)
+        res.status(400).json({ message: err.message })
+    });
 })
 
 module.exports = router
