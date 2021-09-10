c1325b44ccad2461acdaeee0e2dbac03fa2ed927
Mukunda Mhasalkar <mukunda.mhasalkar@ignitesol.com>
2021-09-10 18:11:29 +0530

    Updating app with cors and also added success in response on auth endpoints

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const connectToMongo = require('./db');
 const express = require('express');
+const cors = require("cors");
 
 connectToMongo();
 
@@ -8,11 +9,12 @@ const port = 5000
 
 // Applying middlewares
 app.use(express.json())
+app.use(cors())
 
 // Available routes
 app.use('/api/auth', require('./routes/auth'))
 app.use('/api/notes', require('./routes/notes'))
 
 app.listen(port, () => {
-  console.log(`Example app listening at http://localhost:${port}`)
+  console.log(`iNotebook Backend listening at http://localhost:${port}`)
 })
--- routes/auth.js
+++ routes/auth.js
@@ -15,18 +15,19 @@ router.post('/register', [
     body('password', "Your password should at lease 5 characters long.").isLength({ min: 5 }),
     body('email', "Entered email is not valid.").isEmail()
 ], async (req, res) => {
+    let success = false;
 
     // Finds the validation errors in this request and wraps them in an error message and returns bad request
     const errors = validationResult(req);
     if (!errors.isEmpty()) {
-        res.status(400).json({ errors: errors.array() });
+        res.status(400).json({ success, errors: errors.array() });
     }
 
     try {
         // Check if the user already exists with the email
         let user = await User.findOne({ email: req.body.email })
         if (user) {
-            return res.status(400).json({ error: "User with this E-Mail already exists!" })
+            return res.status(400).json({ success, error: "User with this E-Mail already exists!" })
         }
 
         // Securing the password by hashing
@@ -47,11 +48,12 @@ router.post('/register', [
             }
         }
         const authtoken = jwt.sign(payload, SECRET)
-
-        res.json({authtoken})
+        success = true
+        res.json({success, authtoken})
     } catch (error) {
         console.error(error.message)
-        res.status(500).json({ error: "Internal Server Error" })
+        success = false
+        res.status(500).json({ success, error: "Internal Server Error" })
     }
 })
 
@@ -60,6 +62,7 @@ router.post('/login', [
     body('email', "Entered email is not valid.").isEmail(),
     body('password', "Password cannob be blank").exists()
 ], async (req, res) => {
+    success = false
 
     // Finds the validation errors in this request and wraps them in an error message and returns bad request
     const errors = validationResult(req);
@@ -73,13 +76,13 @@ router.post('/login', [
         // Check if the user exists with the email
         let user = await User.findOne({ email })
         if (!user) {
-            return res.status(400).json({ error: "Your email or password is wrong!" })
+            return res.status(400).json({ success, error: "Your email or password is wrong!" })
         }
 
         // Compare the password with the stored password and authenticate
         let passwordMatched = await bcrypt.compare(password, user.password)
         if (!passwordMatched) {
-            return res.status(400).json({ error: "Your email or password is wrong!" })
+            return res.status(400).json({ success, error: "Your email or password is wrong!" })
         }
 
         // Everything is good!!
@@ -91,24 +94,27 @@ router.post('/login', [
             }
         }
         const authtoken = jwt.sign(payload, SECRET)
-
-        res.json({authtoken})
+        success = true
+        res.json({success, authtoken})
 
     } catch (error) {
         console.error(error.message)
-        res.status(500).json({ error: "Internal Server Error" })
+        success = false
+        res.status(500).json({success, error: "Internal Server Error" })
     }
 })
 
 // ROUTE_3 - Get a user using : GET "/api/auth/get". Login required
 router.get('/getuser', fetchuser, async (req, res) => {
+    success = false
     try {
         let userId = req.user.id;
         const user = await User.findById(userId).select('-password')
-        res.json({user})
+        res.json({success, user})
     } catch (error) {
         console.error(error.message)
-        res.status(500).json({ error: "Internal Server Error" })
+        success = false
+        res.status(500).json({success, error: "Internal Server Error" })
     }
 })
 
